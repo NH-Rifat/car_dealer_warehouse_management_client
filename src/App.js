09452b5840fd16ef8e21177fc16083f794a68bb9
Hasan Rifat <hasanrifatr1@gmail.com>
2022-05-01 01:33:56 +0600

    Manage Inventory section  done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './pages/Header/Header';
 import Home from './pages/Home/Home';
+import ManageInventory from './pages/ManageInventory/ManageInventory';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Home></Home>
+      <Routes>
+      <Route path='/' element={<Home></Home>}></Route>
+      <Route path='/manageInventory' element={<ManageInventory></ManageInventory>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/ManageInventory/ManageInventory.js
@@ -0,0 +1,156 @@
+import React from 'react';
+import styles from './ManageInventory.module.css';
+import car_image1 from '../../images/s_released_cars_thumb01.jpg';
+import car_image2 from '../../images/s_released_cars_thumb02.jpg';
+import car_image3 from '../../images/s_released_cars_thumb03.jpg';
+import car_image4 from '../../images/s_released_cars_thumb04.jpg';
+import car_image5 from '../../images/s_released_cars_thumb05.jpg';
+import car_image6 from '../../images/s_released_cars_thumb06.jpg';
+
+
+const ManageInventory = () => {
+  return (
+    <div className={styles.inventory_container}>
+      <div className={styles.manage_Inventory}>
+        <div className={styles.total_card}>
+          <h2>
+            Total Cars Find:<span> 207</span>
+          </h2>
+        </div>
+        <div className={styles.inventory_card}>
+          <div className={styles.item_img}>
+            <img src={car_image1} alt='' />
+          </div>
+          <div className={styles.inventory_info}>
+            <div className={styles.supplier_price}>
+              <div className={styles.supplier}>
+                <h5 className={styles.supply_heading}>Supplier</h5>
+                <h5 className={styles.supplier_name}>Elon Mask</h5>
+              </div>
+              <div className={styles.price}>
+                <p>
+                  $<span>29,000</span>
+                </p>
+              </div>
+            </div>
+            <div className={styles.calculate_info}>
+              <p>Calculate Financing</p>
+            </div>
+            <div className={styles.inventory_name}>
+              <h3>BMW 8- Series 2-Door</h3>
+            </div>
+            <div className={styles.location_info}>
+              <span className={styles.location_icon}></span>
+              <p>71 - Wast San Park St, New york, NY 98517</p>
+            </div>
+            <div className={styles.border}></div>
+            <div className={styles.other_info}>
+              <div className={styles.delete}>
+                <button>Remove</button>
+              </div>
+              <div className={styles.stock}>
+                <p>
+                  In stock: <span>5</span>
+                </p>
+              </div>
+              <div className={styles.update}>
+                <button>Update</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className={styles.inventory_card}>
+          <div className={styles.item_img}>
+            <img src={car_image2} alt='' />
+          </div>
+          <div className={styles.inventory_info}>
+            <div className={styles.supplier_price}>
+              <div className={styles.supplier}>
+                <h5 className={styles.supply_heading}>Supplier</h5>
+                <h5 className={styles.supplier_name}>Elon Mask</h5>
+              </div>
+              <div className={styles.price}>
+                <p>
+                  $<span>29,000</span>
+                </p>
+              </div>
+            </div>
+            <div className={styles.calculate_info}>
+              <p>Calculate Financing</p>
+            </div>
+            <div className={styles.inventory_name}>
+              <h3>BMW 8- Series 2-Door</h3>
+            </div>
+            <div className={styles.location_info}>
+              <span className={styles.location_icon}></span>
+              <p>71 - Wast San Park St, New york, NY 98517</p>
+            </div>
+            <div className={styles.border}></div>
+            <div className={styles.other_info}>
+              <div className={styles.delete}>
+                <button>Remove</button>
+              </div>
+              <div className={styles.stock}>
+                <p>
+                  In stock: <span>5</span>
+                </p>
+              </div>
+              <div className={styles.update}>
+                <button>Update</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.inventory_card}>
+          <div className={styles.item_img}>
+            <img src={car_image3} alt='' />
+          </div>
+          <div className={styles.inventory_info}>
+            <div className={styles.supplier_price}>
+              <div className={styles.supplier}>
+                <h5 className={styles.supply_heading}>Supplier</h5>
+                <h5 className={styles.supplier_name}>Elon Mask</h5>
+              </div>
+              <div className={styles.price}>
+                <p>
+                  $<span>29,000</span>
+                </p>
+              </div>
+            </div>
+            <div className={styles.calculate_info}>
+              <p>Calculate Financing</p>
+            </div>
+            <div className={styles.inventory_name}>
+              <h3>BMW 8- Series 2-Door</h3>
+            </div>
+            <div className={styles.location_info}>
+              <span className={styles.location_icon}></span>
+              <p>71 - Wast San Park St, New york, NY 98517</p>
+            </div>
+            <div className={styles.border}></div>
+            <div className={styles.other_info}>
+              <div className={styles.delete}>
+                <button>Remove</button>
+              </div>
+              <div className={styles.stock}>
+                <p>
+                  In stock: <span>5</span>
+                </p>
+              </div>
+              <div className={styles.update}>
+                <button>Update</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+  );
+};
+
+export default ManageInventory;
